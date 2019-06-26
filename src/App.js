4b094c6b72799fe66958aca579a115f20c210525
Henrique Tavares <ihenrits@gmail.com>
2019-06-25 23:37:21 -0300

    Organizando estrutura

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 function App() {
-  return <div />;
+  return (
+    <div>
+      <GlobalStyle />
+      <h1>Spotify</h1>
+    </div>
+  );
 }
 
 export default App;
