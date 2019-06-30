4d8a77d93159bf2c74b53128f37e1b73aa7206b1
Henrique Tavares <ihenrits@gmail.com>
2019-06-29 21:06:38 -0300

    Estilizando Player

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+
+import { Wrapper, Container } from './styles/components';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Sidebar />
+      <Wrapper>
+        <Container>
+          <Sidebar />
+        </Container>
+
+        <Player />
+      </Wrapper>
     </>
   );
 }
