e50d1800d6ece34b816d07966bffc6c97fc51fbd
Henrique Tavares <ihenrits@gmail.com>
2019-07-05 00:17:30 -0300

    Exibindo tempo e duraçao

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -20,10 +20,17 @@ import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
 const Player = ({
-  player, play, pause, next, prev,
+  player, play, pause, next, prev, playing, position, duration
 }) => (
   <Container>
-    {!!player.currentSong && <Sound url={player.currentSong.file} playStatus={player.status} onFinishedPlaying={next} />}
+    {!!player.currentSong && (
+      <Sound
+        url={player.currentSong.file}
+        playStatus={player.status}
+        onFinishedPlaying={next}
+        onPlaying={playing}
+      />
+    )}
 
     <Current>
       {!!player.currentSong && (
@@ -64,7 +71,7 @@ const Player = ({
       </Controls>
 
       <Time>
-        <span>1:39</span>
+        <span>{position}</span>
         <ProgressSlider>
           <Slider
             railStyle={{ background: '#404040', borderRadius: 10 }}
@@ -72,7 +79,7 @@ const Player = ({
             handleStyle={{ border: 0 }}
           />
         </ProgressSlider>
-        <span>3:23</span>
+        <span>{duration}</span>
       </Time>
     </Progress>
 
@@ -102,10 +109,24 @@ Player.propTypes = {
   pause: PropTypes.func.isRequired,
   next: PropTypes.func.isRequired,
   prev: PropTypes.func.isRequired,
+  playing: PropTypes.func.isRequired,
+  position: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
 };
+
+function msToTime(duration) {
+  let seconds = parseInt((duration / 1000) % 60, 10);
+  const minutes = parseInt((duration / (1000 * 60)) % 60, 10);
+
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${minutes}:${seconds}`;
+}
 
 const mapStateToProps = state => ({
   player: state.player,
+  position: msToTime(state.player.position),
+  duration: msToTime(state.player.duration)
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
--- src/store/ducks/player.js
+++ src/store/ducks/player.js
@@ -6,12 +6,15 @@ export const Types = {
   PAUSE: 'player/PAUSE',
   NEXT: 'player/NEXT',
   PREV: 'player/PREV',
+  PLAYING: 'player/PLAYING',
 };
 
 const INITIAL_STATE = {
   currentSong: null,
   list: [],
   status: Sound.status.PLAYING,
+  position: null,
+  duration: null,
 };
 
 export default function player(state = INITIAL_STATE, action) {
@@ -45,7 +48,8 @@ export default function player(state = INITIAL_STATE, action) {
       }
 
       return { ...state };
-
+    case Types.PLAYING:
+      return { ...state, ...action.payload };
     default:
       return state;
   }
@@ -64,4 +68,9 @@ export const Creators = {
   next: () => ({ type: Types.NEXT }),
 
   prev: () => ({ type: Types.PREV }),
+
+  playing: ({ position, duration }) => ({
+    type: Types.PLAYING,
+    payload: { position, duration },
+  }),
 };
